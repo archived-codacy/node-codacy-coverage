c31dadf559170dafe0c038e2ccad429cc85dfbf0
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2017-04-18 16:45:14 +0100

    Detect files language and send separate reports per language

--- test/reporter.js
+++ test/reporter.js
@@ -11,7 +11,7 @@
             bodyValidator = Joi.object({
                 total: Joi.number().valid(50),
                 fileReports: Joi.array().items(Joi.object({
-                    filename: Joi.string().valid('filename'),
+                    filename: Joi.string().valid('filename.js'),
                     total: Joi.number().valid(10),
                     coverage: Joi.object({
                         1: Joi.number().valid(1),
@@ -24,7 +24,7 @@
                 total: 50,
                 fileReports: [
                     {
-                        filename: 'filename',
+                        filename: 'filename.js',
                         total: 10,
                         coverage: {
                             1: 1,
